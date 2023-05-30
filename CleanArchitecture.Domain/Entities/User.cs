3ad130673ddb4cfd8cc2d0ca75772292511976b3
Luís Felipe <luisrodrigues@bne.com.br>
2023-05-30 14:48:46 -0300

    Camada de domínio configurada. Entidade base e entidade usuário

---
+++ CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string name, string email) =>
+        (Name, Email) = (name, email);
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+    }
+}
